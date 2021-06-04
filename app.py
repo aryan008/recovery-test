6fb3138d6ad9ff3b9792e879bbefe05b73ca17d4
aryan008 <adam.s.ryan@pwc.com>
2021-06-04 13:58:42 +0000

    connect flask to mongo db

--- app.py
+++ app.py
@@ -9,17 +9,29 @@ os.environ.setdefault("MONGO_URI", "")
 os.environ.setdefault("MONGO_DBNAME", "recovery_test") """
 
 import os
-from flask import Flask
+from flask import (
+    Flask, flash, render_template,
+    redirect, request, session, url_for)
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 if os.path.exists("env.py"):
     import env
 
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+mongo = PyMongo(app)
+
 
 @app.route("/")
-def hello():
-    return "Hello World ... again!"
+@app.route("/get_recovery")
+def get_recovery():
+    recovery = mongo.db.recovery.find()
+    return render_template("recovery.html", recovery=recovery)
 
 
 if __name__ == "__main__":
