7117714f11296a774a04d4eefe4183e3bf238cbc
aryan008 <adam.s.ryan@pwc.com>
2021-06-13 16:12:29 +0000

    working result

--- app.py
+++ app.py
@@ -28,13 +28,13 @@ app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.secret_key = os.environ.get("SECRET_KEY")
 
 ATTRIBUTE_1_DICT ={"No": 2,"Yes - Pool": 3, "Yes - Ice Bath/Sea Swim": 5}
-ATTRIBUTE_2_SCORE = [3,7,10]
-ATTRIBUTE_3_SCORE = [5,10,15]
-ATTRIBUTE_4_SCORE = [8,17,25]
-ATTRIBUTE_5_SCORE = [3,7,10]
-ATTRIBUTE_6_SCORE = [5,10,15]
-ATTRIBUTE_7_SCORE = [3,7,10]
-ATTRIBUTE_8_SCORE = [3,7,10]
+ATTRIBUTE_2_DICT ={"Not at all": 3,"Somewhat nutritious": 7, "Very nutritious": 10}
+ATTRIBUTE_3_DICT ={"Yes - Tough session(s)": 5,"Yes - Light Session(s)": 10, "No": 15}
+ATTRIBUTE_4_DICT ={"Less than 6 hours": 8,"6-7.5 hours": 17, "7.5+ hours": 25}
+ATTRIBUTE_5_DICT ={"Exhausted/Tired": 3,"Ok": 7, "Good/Fresh": 10}
+ATTRIBUTE_6_DICT ={"<1 Litre": 5,"1-3 Litres": 10, "3+ Litres": 15}
+ATTRIBUTE_7_DICT ={"No": 3,"Yes - Less than 10 mins": 7, "Yes - More than 10 mins": 10}
+ATTRIBUTE_8_DICT ={"No": 3,"Yes - Less than 10 mins": 7, "Yes - More than 10 mins": 10}
 
 mongo = PyMongo(app)
 
@@ -151,8 +151,16 @@ def get_result():
     latest_entry = mongo.db.entries.find_one( {"$query":{}, "$orderby":{"$natural":-1}} )
     full_pycache = list(latest_entry.items())
     selection_list = full_pycache[1][1]
+    total = 0
+
     attr_1_query = selection_list[0]
     attr_2_query = selection_list[1]
+    attr_3_query = selection_list[2]
+    attr_4_query = selection_list[3]
+    attr_5_query = selection_list[4]
+    attr_6_query = selection_list[5]
+    attr_7_query = selection_list[6]
+    attr_8_query = selection_list[7]
 
     if attr_1_query == list(ATTRIBUTE_1_DICT.keys())[0]:
         attr_1_result = list(ATTRIBUTE_1_DICT.values())[0]
@@ -160,15 +168,73 @@ def get_result():
         attr_1_result = list(ATTRIBUTE_1_DICT.values())[1]
     elif attr_1_query == list(ATTRIBUTE_1_DICT.keys())[2]:
         attr_1_result = list(ATTRIBUTE_1_DICT.values())[2]
+    total += attr_1_result
     
-    if attr_2_query == "Not at all":
-        attr_2_result = 3
-    elif attr_2_query == "Somewhat nutritious":
-        attr_2_result = 7
-    elif attr_2_query == "Very nutritious":
-        attr_2_result = 10
+    if attr_2_query == list(ATTRIBUTE_2_DICT.keys())[0]:
+        attr_2_result = list(ATTRIBUTE_2_DICT.values())[0]
+    elif attr_2_query == list(ATTRIBUTE_2_DICT.keys())[1]:
+        attr_2_result = list(ATTRIBUTE_2_DICT.values())[1]
+    elif attr_2_query == list(ATTRIBUTE_2_DICT.keys())[2]:
+        attr_2_result = list(ATTRIBUTE_2_DICT.values())[2]
+    total += attr_2_result
+
+    if attr_3_query == list(ATTRIBUTE_3_DICT.keys())[0]:
+        attr_3_result = list(ATTRIBUTE_3_DICT.values())[0]
+    elif attr_3_query == list(ATTRIBUTE_3_DICT.keys())[1]:
+        attr_3_result = list(ATTRIBUTE_3_DICT.values())[1]
+    elif attr_3_query == list(ATTRIBUTE_3_DICT.keys())[2]:
+        attr_3_result = list(ATTRIBUTE_3_DICT.values())[2]
+    total += attr_3_result
+
+    if attr_4_query == list(ATTRIBUTE_4_DICT.keys())[0]:
+        attr_4_result = list(ATTRIBUTE_4_DICT.values())[0]
+    elif attr_4_query == list(ATTRIBUTE_4_DICT.keys())[1]:
+        attr_4_result = list(ATTRIBUTE_4_DICT.values())[1]
+    elif attr_4_query == list(ATTRIBUTE_4_DICT.keys())[2]:
+        attr_4_result = list(ATTRIBUTE_4_DICT.values())[2]
+    total += attr_4_result
+
+    if attr_5_query == list(ATTRIBUTE_5_DICT.keys())[0]:
+        attr_5_result = list(ATTRIBUTE_5_DICT.values())[0]
+    elif attr_5_query == list(ATTRIBUTE_5_DICT.keys())[1]:
+        attr_5_result = list(ATTRIBUTE_5_DICT.values())[1]
+    elif attr_5_query == list(ATTRIBUTE_5_DICT.keys())[2]:
+        attr_5_result = list(ATTRIBUTE_5_DICT.values())[2]
+    total += attr_5_result
+
+    if attr_6_query == list(ATTRIBUTE_6_DICT.keys())[0]:
+        attr_6_result = list(ATTRIBUTE_6_DICT.values())[0]
+    elif attr_6_query == list(ATTRIBUTE_6_DICT.keys())[1]:
+        attr_6_result = list(ATTRIBUTE_6_DICT.values())[1]
+    elif attr_6_query == list(ATTRIBUTE_6_DICT.keys())[2]:
+        attr_6_result = list(ATTRIBUTE_6_DICT.values())[2]
+    total += attr_6_result
+
+    if attr_7_query == list(ATTRIBUTE_7_DICT.keys())[0]:
+        attr_7_result = list(ATTRIBUTE_7_DICT.values())[0]
+    elif attr_7_query == list(ATTRIBUTE_7_DICT.keys())[1]:
+        attr_7_result = list(ATTRIBUTE_7_DICT.values())[1]
+    elif attr_7_query == list(ATTRIBUTE_7_DICT.keys())[2]:
+        attr_7_result = list(ATTRIBUTE_7_DICT.values())[2]
+    total += attr_7_result
+
+    if attr_8_query == list(ATTRIBUTE_8_DICT.keys())[0]:
+        attr_8_result = list(ATTRIBUTE_8_DICT.values())[0]
+    elif attr_8_query == list(ATTRIBUTE_8_DICT.keys())[1]:
+        attr_8_result = list(ATTRIBUTE_8_DICT.values())[1]
+    elif attr_8_query == list(ATTRIBUTE_8_DICT.keys())[2]:
+        attr_8_result = list(ATTRIBUTE_8_DICT.values())[2]
+    total += attr_8_result
     
     print(attr_1_result)
+    print(attr_2_result)
+    print(attr_3_result)
+    print(attr_4_result)
+    print(attr_5_result)
+    print(attr_6_result)
+    print(attr_7_result)
+    print(attr_8_result)
+    print("Total is: ", total,"% recovered")
 
 
 if __name__ == "__main__":
