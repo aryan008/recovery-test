7fdf390c1a9a7bdb20ca7b57d4790cf8361c0f0b
aryan008 <adam.s.ryan@pwc.com>
2021-06-19 08:50:35 +0000

    bulletproof new entry

--- app.py
+++ app.py
@@ -275,103 +275,126 @@ def about():
 
 @app.route("/new_entry", methods=["GET", "POST"])
 def new_entry():
-    if request.method == "POST":
+    try:
         username = mongo.db.users.find_one(
-        {"username": session["user"]})["username"]
-        result = get_result(username)
+                {"username": session["user"]})["username"]
 
-        final_attributes = request.form.getlist("options.choice")
-        total = 0
+        latest_entry_date = mongo.db.entries.find({"created_by": username}).sort(username, -1)
+            
+        initial_list_date = list(latest_entry_date)
+        last_entry_date = initial_list_date[-1]
+        last_entry_list_date = list(last_entry_date.items())
+        final_date = last_entry_list_date[3][1]
 
-        attr_1_query = final_attributes[0]
-        attr_2_query = final_attributes[1]
-        attr_3_query = final_attributes[2]
-        attr_4_query = final_attributes[3]
-        attr_5_query = final_attributes[4]
-        attr_6_query = final_attributes[5]
-        attr_7_query = final_attributes[6]
-        attr_8_query = final_attributes[7]
+        today = datetime.today().strftime('%Y-%m-%d')
+        new_entry = datetime.strptime(final_date, "%Y-%m-%d")
+        new_today = datetime.strptime(today, "%Y-%m-%d")
 
-        if attr_1_query == list(ATTRIBUTE_1_DICT.keys())[0]:
-            attr_1_result = list(ATTRIBUTE_1_DICT.values())[0]
-        elif attr_1_query == list(ATTRIBUTE_1_DICT.keys())[1]:
-            attr_1_result = list(ATTRIBUTE_1_DICT.values())[1]
-        elif attr_1_query == list(ATTRIBUTE_1_DICT.keys())[2]:
-            attr_1_result = list(ATTRIBUTE_1_DICT.values())[2]
-        total += attr_1_result
-        
-        if attr_2_query == list(ATTRIBUTE_2_DICT.keys())[0]:
-            attr_2_result = list(ATTRIBUTE_2_DICT.values())[0]
-        elif attr_2_query == list(ATTRIBUTE_2_DICT.keys())[1]:
-            attr_2_result = list(ATTRIBUTE_2_DICT.values())[1]
-        elif attr_2_query == list(ATTRIBUTE_2_DICT.keys())[2]:
-            attr_2_result = list(ATTRIBUTE_2_DICT.values())[2]
-        total += attr_2_result
+        # https://stackoverflow.com/questions/8419564/difference-between-two-dates-in-python
+        date_difference = abs((new_today - new_entry).days)
 
-        if attr_3_query == list(ATTRIBUTE_3_DICT.keys())[0]:
-            attr_3_result = list(ATTRIBUTE_3_DICT.values())[0]
-        elif attr_3_query == list(ATTRIBUTE_3_DICT.keys())[1]:
-            attr_3_result = list(ATTRIBUTE_3_DICT.values())[1]
-        elif attr_3_query == list(ATTRIBUTE_3_DICT.keys())[2]:
-            attr_3_result = list(ATTRIBUTE_3_DICT.values())[2]
-        total += attr_3_result
+        if date_difference ==0:
+            options = mongo.db.recovery.find()
+            return render_template("new_entry.html", options = options, date_difference=date_difference)
 
-        if attr_4_query == list(ATTRIBUTE_4_DICT.keys())[0]:
-            attr_4_result = list(ATTRIBUTE_4_DICT.values())[0]
-        elif attr_4_query == list(ATTRIBUTE_4_DICT.keys())[1]:
-            attr_4_result = list(ATTRIBUTE_4_DICT.values())[1]
-        elif attr_4_query == list(ATTRIBUTE_4_DICT.keys())[2]:
-            attr_4_result = list(ATTRIBUTE_4_DICT.values())[2]
-        total += attr_4_result
+    except IndexError as error:
+        if request.method == "POST":
+            username = mongo.db.users.find_one(
+            {"username": session["user"]})["username"]
+            result = get_result(username)
 
-        if attr_5_query == list(ATTRIBUTE_5_DICT.keys())[0]:
-            attr_5_result = list(ATTRIBUTE_5_DICT.values())[0]
-        elif attr_5_query == list(ATTRIBUTE_5_DICT.keys())[1]:
-            attr_5_result = list(ATTRIBUTE_5_DICT.values())[1]
-        elif attr_5_query == list(ATTRIBUTE_5_DICT.keys())[2]:
-            attr_5_result = list(ATTRIBUTE_5_DICT.values())[2]
-        total += attr_5_result
+            final_attributes = request.form.getlist("options.choice")
+            total = 0
 
-        if attr_6_query == list(ATTRIBUTE_6_DICT.keys())[0]:
-            attr_6_result = list(ATTRIBUTE_6_DICT.values())[0]
-        elif attr_6_query == list(ATTRIBUTE_6_DICT.keys())[1]:
-            attr_6_result = list(ATTRIBUTE_6_DICT.values())[1]
-        elif attr_6_query == list(ATTRIBUTE_6_DICT.keys())[2]:
-            attr_6_result = list(ATTRIBUTE_6_DICT.values())[2]
-        total += attr_6_result
+            attr_1_query = final_attributes[0]
+            attr_2_query = final_attributes[1]
+            attr_3_query = final_attributes[2]
+            attr_4_query = final_attributes[3]
+            attr_5_query = final_attributes[4]
+            attr_6_query = final_attributes[5]
+            attr_7_query = final_attributes[6]
+            attr_8_query = final_attributes[7]
 
-        if attr_7_query == list(ATTRIBUTE_7_DICT.keys())[0]:
-            attr_7_result = list(ATTRIBUTE_7_DICT.values())[0]
-        elif attr_7_query == list(ATTRIBUTE_7_DICT.keys())[1]:
-            attr_7_result = list(ATTRIBUTE_7_DICT.values())[1]
-        elif attr_7_query == list(ATTRIBUTE_7_DICT.keys())[2]:
-            attr_7_result = list(ATTRIBUTE_7_DICT.values())[2]
-        total += attr_7_result
+            if attr_1_query == list(ATTRIBUTE_1_DICT.keys())[0]:
+                attr_1_result = list(ATTRIBUTE_1_DICT.values())[0]
+            elif attr_1_query == list(ATTRIBUTE_1_DICT.keys())[1]:
+                attr_1_result = list(ATTRIBUTE_1_DICT.values())[1]
+            elif attr_1_query == list(ATTRIBUTE_1_DICT.keys())[2]:
+                attr_1_result = list(ATTRIBUTE_1_DICT.values())[2]
+            total += attr_1_result
+            
+            if attr_2_query == list(ATTRIBUTE_2_DICT.keys())[0]:
+                attr_2_result = list(ATTRIBUTE_2_DICT.values())[0]
+            elif attr_2_query == list(ATTRIBUTE_2_DICT.keys())[1]:
+                attr_2_result = list(ATTRIBUTE_2_DICT.values())[1]
+            elif attr_2_query == list(ATTRIBUTE_2_DICT.keys())[2]:
+                attr_2_result = list(ATTRIBUTE_2_DICT.values())[2]
+            total += attr_2_result
 
-        if attr_8_query == list(ATTRIBUTE_8_DICT.keys())[0]:
-            attr_8_result = list(ATTRIBUTE_8_DICT.values())[0]
-        elif attr_8_query == list(ATTRIBUTE_8_DICT.keys())[1]:
-            attr_8_result = list(ATTRIBUTE_8_DICT.values())[1]
-        elif attr_8_query == list(ATTRIBUTE_8_DICT.keys())[2]:
-            attr_8_result = list(ATTRIBUTE_8_DICT.values())[2]
-        total += attr_8_result
+            if attr_3_query == list(ATTRIBUTE_3_DICT.keys())[0]:
+                attr_3_result = list(ATTRIBUTE_3_DICT.values())[0]
+            elif attr_3_query == list(ATTRIBUTE_3_DICT.keys())[1]:
+                attr_3_result = list(ATTRIBUTE_3_DICT.values())[1]
+            elif attr_3_query == list(ATTRIBUTE_3_DICT.keys())[2]:
+                attr_3_result = list(ATTRIBUTE_3_DICT.values())[2]
+            total += attr_3_result
+
+            if attr_4_query == list(ATTRIBUTE_4_DICT.keys())[0]:
+                attr_4_result = list(ATTRIBUTE_4_DICT.values())[0]
+            elif attr_4_query == list(ATTRIBUTE_4_DICT.keys())[1]:
+                attr_4_result = list(ATTRIBUTE_4_DICT.values())[1]
+            elif attr_4_query == list(ATTRIBUTE_4_DICT.keys())[2]:
+                attr_4_result = list(ATTRIBUTE_4_DICT.values())[2]
+            total += attr_4_result
+
+            if attr_5_query == list(ATTRIBUTE_5_DICT.keys())[0]:
+                attr_5_result = list(ATTRIBUTE_5_DICT.values())[0]
+            elif attr_5_query == list(ATTRIBUTE_5_DICT.keys())[1]:
+                attr_5_result = list(ATTRIBUTE_5_DICT.values())[1]
+            elif attr_5_query == list(ATTRIBUTE_5_DICT.keys())[2]:
+                attr_5_result = list(ATTRIBUTE_5_DICT.values())[2]
+            total += attr_5_result
+
+            if attr_6_query == list(ATTRIBUTE_6_DICT.keys())[0]:
+                attr_6_result = list(ATTRIBUTE_6_DICT.values())[0]
+            elif attr_6_query == list(ATTRIBUTE_6_DICT.keys())[1]:
+                attr_6_result = list(ATTRIBUTE_6_DICT.values())[1]
+            elif attr_6_query == list(ATTRIBUTE_6_DICT.keys())[2]:
+                attr_6_result = list(ATTRIBUTE_6_DICT.values())[2]
+            total += attr_6_result
+
+            if attr_7_query == list(ATTRIBUTE_7_DICT.keys())[0]:
+                attr_7_result = list(ATTRIBUTE_7_DICT.values())[0]
+            elif attr_7_query == list(ATTRIBUTE_7_DICT.keys())[1]:
+                attr_7_result = list(ATTRIBUTE_7_DICT.values())[1]
+            elif attr_7_query == list(ATTRIBUTE_7_DICT.keys())[2]:
+                attr_7_result = list(ATTRIBUTE_7_DICT.values())[2]
+            total += attr_7_result
+
+            if attr_8_query == list(ATTRIBUTE_8_DICT.keys())[0]:
+                attr_8_result = list(ATTRIBUTE_8_DICT.values())[0]
+            elif attr_8_query == list(ATTRIBUTE_8_DICT.keys())[1]:
+                attr_8_result = list(ATTRIBUTE_8_DICT.values())[1]
+            elif attr_8_query == list(ATTRIBUTE_8_DICT.keys())[2]:
+                attr_8_result = list(ATTRIBUTE_8_DICT.values())[2]
+            total += attr_8_result
 
 
-        entry = {
-            "option_choice": request.form.getlist("options.choice"),
-            "created_by": session["user"],
-            "user_chosen_date": request.form.get("date_choice"),
-            "submission_date": datetime.today().strftime('%Y-%m-%d'),
-            "comment_text": request.form.get("comment_text"),
-            "name": mongo.db.users.find_one({"username": session["user"]})["_id"],
-            "score": total
-        }
-        mongo.db.entries.insert_one(entry)
-        flash("Task Successfully Added")
-        return redirect(url_for("profile", username=username))
+            entry = {
+                "option_choice": request.form.getlist("options.choice"),
+                "created_by": session["user"],
+                "user_chosen_date": request.form.get("date_choice"),
+                "submission_date": datetime.today().strftime('%Y-%m-%d'),
+                "comment_text": request.form.get("comment_text"),
+                "name": mongo.db.users.find_one({"username": session["user"]})["_id"],
+                "score": total
+            }
+            mongo.db.entries.insert_one(entry)
+            flash("Task Successfully Added")
+            return redirect(url_for("profile", username=username))
 
-    options = mongo.db.recovery.find()
-    return render_template("new_entry.html", options = options)
+        options = mongo.db.recovery.find()
+        return render_template("new_entry.html", options = options)
 
 
 def get_date(username):
