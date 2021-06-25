1db66e2b1eeab4238be88a27636c74053afb09d6
aryan008 <adam.s.ryan@pwc.com>
2021-06-25 12:21:25 +0000

    profile done

--- static/js/script.js
+++ static/js/script.js
@@ -23,3 +23,22 @@ $('.score-percentage').each(function() {
         $(this).addClass( "low" );
     }
 }); 
+
+
+$('.result-container').each(function() {
+    let userScore = $(".inner-container p");
+    
+    let splitScore = userScore.text().split("%")
+    
+    let actualScore = parseInt(splitScore[0])
+    console.log(actualScore)
+    
+
+    if (actualScore >=70) {
+        $(this).addClass( "recovered" );
+    } else if (actualScore >= 50) {
+        $(this).addClass( "moderate" );
+    } else {
+        $(this).addClass( "low" );
+    }
+}); 
