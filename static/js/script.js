ec7a4e77d7bfc58d84ad0cc61218ecfcee68cca9
aryan008 <adam.s.ryan@pwc.com>
2021-06-30 08:51:39 +0000

    documenting JS file

--- static/js/script.js
+++ static/js/script.js
@@ -1,20 +1,33 @@
+/**
+* @fileOverview Recovery Pod script.js file.
+* @author <a href="https://github.com/aryan008">Adam Ryan</a>
+* @version 1.1.1
+*/
+/*jshint esversion: 6 */
+
+/* The below function adds the class of "active" to the current window location the site user is on */
 // https://codepen.io/figarali/pen/araWdP
-jQuery(function($) {
- let path = window.location.href;
- $('ul a').each(function() {
-  if (this.href === path) {
-   $(this).addClass('active');
-  }
- });
+jQuery(function ($) {
+    // find the window location the user is currently on
+    let path = window.location.href;
+    // for loop to apply a function to each "ul a" tag
+    $('ul a').each(function () {
+        if (this.href === path) {
+            // add the class of active if this href is the window location
+            $(this).addClass('active');
+        }
+    });
 });
 
-
+/* The below function applies the correct class to the score of the user for all entries */
 $('.score-percentage').each(function() {
     let tableScore = $(this);
+    // get the score of each entry
     let score = tableScore.text()
+    // convert each score to numerical
     let numberScore = parseInt(score)
     
-
+    // apply appropriate classes depending on the score
     if (numberScore >=70) {
         $(this).addClass( "recovered" );
     } else if (numberScore >= 50) {
@@ -24,16 +37,16 @@ $('.score-percentage').each(function() {
     }
 }); 
 
-
+/* The below function applies the correct class to the score of the user for todays entry */
 $('.result-container').each(function() {
+    // grab the result the user got
     let userScore = $(".inner-container p");
-    
+    // get rid of the percent sign
     let splitScore = userScore.text().split("%")
-    
+    // convert the score to an integer
     let actualScore = parseInt(splitScore[0])
-    console.log(actualScore)
     
-
+    // apply appropriate classes depending on the score
     if (actualScore >=70) {
         $(this).addClass( "recovered" );
     } else if (actualScore >= 50) {
